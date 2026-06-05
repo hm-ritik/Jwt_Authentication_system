4e9ba60e444e29a05817c6de9db220fecf4be7e6
hm-ritik <ritiksharma98010@gmail.com>
2026-06-05 23:11:05 +0530

    Authorization makes me cry today

--- app/api/apis.py
+++ app/api/apis.py
@@ -3,6 +3,8 @@ from app.schemas.design import UserResponse , RegisterUser , Login
 from app.core.database import get_db
 from sqlalchemy.orm import Session 
 from app.crud.crudop import register , readuser ,loginuser
+from app.api.auth import get_current_user
+
 
 
 
@@ -15,12 +17,13 @@ def registeruser(post:RegisterUser , db:Session=Depends(get_db)):
         raise HTTPException(status_code=404 , detail="User Already Exists")
     return user
 
-@router.get("/readuser{id}",response_model=UserResponse)
-def showuser(id:int , db:Session=Depends(get_db)):
+@router.get("/readuser/{id}",response_model=UserResponse)
+def showuser(id:int , db:Session=Depends(get_db),current_user=Depends(get_current_user)):
     user=readuser(id , db)
     if user is None:
         raise HTTPException(status_code=404 , detail="user do not exists")
     return user
+
 @router.post("/login/")
 def userlogin(post:Login , db:Session=Depends(get_db)):
     user=loginuser(post , db)
@@ -28,3 +31,8 @@ def userlogin(post:Login , db:Session=Depends(get_db)):
     if user is None:
         raise HTTPException(status_code=404 , detail="Invalid Username or Password")
     return user
+
+
+ 
+
+
